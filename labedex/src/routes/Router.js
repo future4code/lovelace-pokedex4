694555c4c691fc94f3fc0b65f447924633b475ea
Rafael Machado <rafaelxw7@gmail.com>
2021-08-23 16:10:13 -0300

    Criado Router, Coordinator e GlobalStyles

---
+++ labedex/src/routes/Router.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Details from "../pages/Details/Details"
+import Home from '../pages/Home/Home'
+import Pokedex from "../pages/Pokemon/Pokedex"
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/pokedex" component={Pokedex} />
+                <Route exact path="/details" component={Details} />
+                <Route>
+                    <div>Erro 404 - Página não encontrada</div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router
