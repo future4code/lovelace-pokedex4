29275fd0f3bd96bbb3a90b583c56096ddf2f71a8
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-09-15 14:02:46 -0300

    Fix - Site scrollando lateralmente

--- labedex/src/globalStyles.js
+++ labedex/src/globalStyles.js
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
+    overflow-x: hidden;
   }
 `
 export default GlobalStyle
